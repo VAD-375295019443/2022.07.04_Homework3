f840ae2cc8555653901d851f5131fd8431b38524
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-06-21 13:53:10 +0300

    Add project files.

---
+++ Airline/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Airline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+    }
+
+
+
+
+
+
+    public class claAirLine
+    {
+        public int      intId; //Код.
+        public string   strName; //Название авиакомпании.
+    }
+
+
+    public class claAirPlane
+    {
+        public int      intLinkCode; //Код связи.
+        public string   strName; //Название самолета (тип).
+        public int      intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
+        public double   dblCargoCapacity; //Вместимость груза (кг).
+        public double   dblFlightRange; //Дальность полета (км).
+        public int      intFuelConsumption; //Количество потребляемого топлива (л).
+    }
+}
