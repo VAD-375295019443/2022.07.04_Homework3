9fdf177d06760eaf7ca881032f973891e17e8085
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-06-21 22:18:36 +0300

    Changes

--- Airline/Program.cs
+++ Airline/Program.cs
@@ -4,10 +4,16 @@ namespace Airline
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            while (1 == 1)
+            while (true)
             {
+                List<claAirLine> AirLine = new List<claAirLine>(); //Создаем коллекцию авиакомпаний.
+                List<claAirPlane> AirPlane = new List<claAirPlane>(); //Создаем коллекцию самолетов.
+                //Сюда засунуть данные из файла.
+
+
+
                 Console.Clear();
                 Console.WriteLine("РЕЕСТР МЕЖДУНАРОДНЫХ АВИАКОМПАНИЙ.");
                 Console.WriteLine();
@@ -15,6 +21,7 @@ namespace Airline
                 Console.WriteLine("1 - Создание авиакомпании.");
                 Console.WriteLine("2 - Редактирование авиакомпании.");
                 Console.WriteLine("3 - Анализ показателей авиакомпании.");
+                Console.WriteLine("4 - Удаление авиакомпании.");
                 Console.WriteLine("Для выхода наберите Exit.");
                 Console.WriteLine();
 
@@ -22,31 +29,25 @@ namespace Airline
 
                 if (strMenuNumber == "1")
                 {
-
-                    //Вы вошли в раздел создания авиакомпаний.
                     //Метод создания авиакомпании.
+                    F_voiCreateAirline(ref AirLine, AirPlane);
 
 
-                    
+
 
 
                 }
                 else if (strMenuNumber == "2")
                 {
-
-
-
-                    //Вы вошли в раздел редактирования авиакомпаний.
                     //Метод редактирования авиакомпани.
-
                 }
                 else if (strMenuNumber == "3")
                 {
-                    //Вы вошли в раздел анализа показателей авиакомпаний.
                     //Метод анализа показателей авиакомпании.
-
-
-                    //Вся информация о авиакомпаниях, находящихся в составе реестра.
+                }
+                else if (strMenuNumber == "4")
+                {
+                    //Метод удаления авиакомпании.
                 }
                 else if (strMenuNumber == "Exit" || strMenuNumber == "exit")
                 {
@@ -56,43 +57,90 @@ namespace Airline
                 }
                 else
                 {
-                    Console.Clear(); 
+                    Console.Clear();
                     Console.WriteLine("Вы ввели не корректные данные.");
                     Console.WriteLine("Для продолжения нажмите Enter.");
-                    Console.WriteLine(); 
+                    Console.WriteLine();
                     strMenuNumber = Console.ReadLine();
                     continue;
                 }
 
 
-
-
-
-
-
             }
         }
-    }
+
+
+
+
+        //Метод создания авиакомпании.
+        public static void F_voiCreateAirline(ref List<claAirLine> AirLine, List<claAirPlane> AirPlane)
+        {
+            Console.Clear();
+            Console.WriteLine("Вы находитесь в разделе создания авиакомпаний.");
+            Console.WriteLine();
+
+            
 
 
 
 
 
 
-    public class claAirLine
-    {
-        public int      intId; //Код.
-        public string   strName; //Название авиакомпании.
-    }
 
 
-    public class claAirPlane
-    {
-        public int      intLinkCode; //Код связи.
-        public string   strName; //Название самолета (тип).
-        public int      intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
-        public double   dblCargoCapacity; //Вместимость груза (кг).
-        public double   dblFlightRange; //Дальность полета (км).
-        public int      intFuelConsumption; //Количество потребляемого топлива (л).
+        }
+
+
+        //Метод редактирования авиакомпании.
+        public static void F_voiEditAirline()
+        {
+            Console.Clear();
+            Console.WriteLine("Вы находитесь в разделе редактирования авиакомпаний.");
+            Console.WriteLine();
+
+        }
+
+
+        //Метод анализа авиакомпании.
+        public static void F_voiAnalysisAirline()
+        {
+            Console.Clear();
+            Console.WriteLine("Вы находитесь в разделе анализа показателей авиакомпаний.");
+            Console.WriteLine();
+            
+            
+            
+            //Вся информация о авиакомпаниях, зарегистрированных в реестре.
+        }
+
+
+        //Метод удаления авиакомпании.
+        public static void F_voiDeleteAirline()
+        {
+            Console.Clear();
+            Console.WriteLine("Вы находитесь в разделе удаления авиакомпаний.");
+            Console.WriteLine();
+
+        }
+
+
+        //Класс авиакомпаний.
+        public class claAirLine
+        {
+            public int intId; //Код.
+            public string strName; //Название авиакомпании.
+        }
+
+
+        //Класс самолетов.
+        public class claAirPlane
+        {
+            public int intLinkCode; //Код связи.
+            public string strName; //Название самолета (тип).
+            public int intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
+            public double dblCargoCapacity; //Вместимость груза (кг).
+            public double dblFlightRange; //Дальность полета (км).
+            public int intFuelConsumption; //Количество потребляемого топлива (л).
+        }
     }
 }
