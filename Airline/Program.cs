477de6fadc3d2ff5fe082484f58462f89fca9e18
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-06-28 21:11:52 +0300

    Changes

--- Airline/Program.cs
+++ Airline/Program.cs
@@ -7,16 +7,10 @@ namespace Airline
         
         public static void Main(string[] args)
         {
-            List<claAirline> Airline = new List<claAirline>(); //Создаем коллекцию авиакомпаний.
             List<claAirplane> DatabaseAirplane = new List<claAirplane>(); //Создаем коллекцию базы данных воздушных судов.
+            F_ReadDatabaseAirplane(ref DatabaseAirplane); //Считываем базу данных воздушных судов из файла в коллекцию.
 
-
-
-
-            //File.AppendAllText(strPath, "\n" + "Hello");
-            //File.AppendAllText(strPath, "\n" + "aaaaa");
-            //File.AppendAllText(strPath, "\n" + "bbbbbb");
-
+            List<claAirline> Airline = new List<claAirline>(); //Создаем коллекцию авиакомпаний.
 
 
 
@@ -35,6 +29,7 @@ namespace Airline
                 Console.WriteLine("2 - Редактирование авиакомпаний.");
                 Console.WriteLine("3 - Анализ показателей авиакомпаний.");
                 Console.WriteLine("4 - Удаление авиакомпаний.");
+                Console.WriteLine("5 - Редактирование базы данных воздушных судов.");
                 Console.WriteLine("Exit - Выход.");
                 Console.WriteLine();
 
@@ -61,6 +56,10 @@ namespace Airline
                 {
                     //Метод удаления авиакомпаний.
                 }
+                else if (strMenuNumber == "5")
+                {
+                    F_DatabaseAirplane(ref DatabaseAirplane); //Метод редактирования базы данных воздушных судов.
+                }
                 else if (strMenuNumber == "Exit" || strMenuNumber == "exit")
                 {
                     Console.Clear();
@@ -75,6 +74,111 @@ namespace Airline
 
             }
         }
+
+
+
+
+        //Метод редактирования базы данных воздушных судов.
+        public static void F_DatabaseAirplane(ref List<claAirplane> DatabaseAirplane)
+        {
+            int intAirplaneNumber; //Номер воздушного судна.
+
+            string strName; //Название самолета (тип).
+            int intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
+            double dblCargoCapacity; //Вместимость груза (кг).
+            double dblFlightRange; //Дальность полета (км).
+            double dblFuelConsumption; //Количество потребляемого топлива (л).
+
+            string? strMenuNumber; //Номер меню.
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("РЕЕСТР МЕЖДУНАРОДНЫХ АВИАКОМПАНИЙ.");
+                Console.WriteLine();
+                Console.WriteLine("Вы находитесь в разделе редактирования базы данных воздушных судов.");
+                Console.WriteLine();
+
+                if (DatabaseAirplane.Count == 0)
+                {
+                    Console.WriteLine("В базе данных нет воздушных судов.");
+                }
+                else
+                {
+                    for (int int1 = 0; int1 <= DatabaseAirplane.Count - 1; int1++)
+                    {
+                        Console.WriteLine("Воздушное судно:");
+                        Console.WriteLine($"{int1} - {DatabaseAirplane[int1].strName}");
+                        Console.WriteLine($"     Параметры:"); ;
+                        Console.WriteLine($"     Вместимость пассажиров (количество посадочных мест): {DatabaseAirplane[int1].intPassengerCapacity}");
+                        Console.WriteLine($"     Вместимость груза (кг): {DatabaseAirplane[int1].dblCargoCapacity}");
+                        Console.WriteLine($"     Дальность полета (км): {DatabaseAirplane[int1].dblFlightRange}");
+                        Console.WriteLine($"     Количество потребляемого топлива (л): {DatabaseAirplane[int1].dblFuelConsumption}");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Введите номер пункта меню:");
+                Console.WriteLine("1 - Добавление воздушного судна.");
+                Console.WriteLine("2 - Удаление воздушного судна.");
+                Console.WriteLine("Exit - Возврат в предыдущее меню.");
+                Console.WriteLine();
+
+                strMenuNumber = Console.ReadLine();
+                Console.WriteLine();
+
+                if (strMenuNumber == "1")
+                {
+                    Console.WriteLine("Введите наименование воздушного судна:");
+                    strName = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Введите вместимость пассажиров (количество посадочных мест):");
+                    intPassengerCapacity = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Введите вместимость груза (кг):");
+                    dblCargoCapacity = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Введите дальность полета (км):");
+                    dblFlightRange = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("Введите количество потребляемого топлива (л):");
+                    dblFuelConsumption = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+
+                    DatabaseAirplane.Add(new claAirplane(strName, intPassengerCapacity, dblCargoCapacity, dblFlightRange, dblFuelConsumption));
+
+                    F_WriteDatabaseAirplane(DatabaseAirplane);
+                }
+                else if (strMenuNumber == "2")
+                {
+                    Console.WriteLine("Введите номер воздушного судна:");
+                    intAirplaneNumber = Convert.ToInt32(Console.ReadLine());
+
+                    if(intAirplaneNumber >= 0 && intAirplaneNumber <= DatabaseAirplane.Count) //Если такой номер воздушного судна есть.
+                    {
+                        DatabaseAirplane.RemoveRange(intAirplaneNumber, 1); //Удаляем воздушное судно под этим номером.
+
+                        F_WriteDatabaseAirplane(DatabaseAirplane); //Перезаписываем коллекцию в файл.
+                    }
+                }
+                else if (strMenuNumber == "Exit" || strMenuNumber == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
 
 
 
@@ -191,7 +295,6 @@ namespace Airline
 
 
 
-            //Airline[Airline.Count - 1].Airplane.Add(new claAirplane("hjkh", 14, 12.5, 45.3, 56));
 
         }
 
@@ -235,100 +338,57 @@ namespace Airline
 
 
 
-        //Метод базы данных воздушных судов.
-        public static void F_DatabaseAirplane(ref List<claAirplane> DatabaseAirplane)
-        {
-            Console.Clear();
-            Console.WriteLine("РЕЕСТР МЕЖДУНАРОДНЫХ АВИАКОМПАНИЙ.");
-            Console.WriteLine();
-            Console.WriteLine("Вы находитесь в разделе базы данных воздушных судов.");
-            Console.WriteLine();
 
-            if (DatabaseAirplane.Count == 0)
+
+
+
+
+        //Считываем базу данных воздушных судов из файла в коллекцию.
+        public static void F_ReadDatabaseAirplane(ref List<claAirplane> DatabaseAirplane)
+        {
+            string strPath = @"d:\Registry\DatabaseAirplane";
+
+            if (Directory.Exists(strPath) == false) //Если директории не существует.
             {
-                Console.WriteLine("В базе данных нет воздушных судов.");
+                Directory.CreateDirectory(strPath); //Создаем.
             }
             else
             {
-                for (int int1=0; int1<=DatabaseAirplane.Count-1; int1++)
+                strPath = @"d:\Registry\DatabaseAirplane\DatabaseAirplane.txt";
+
+                string[] arrstrDatabaseAirplane = new string[0]; ;
+
+                string strName; //Название самолета (тип).
+                int intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
+                double dblCargoCapacity; //Вместимость груза (кг).
+                double dblFlightRange; //Дальность полета (км).
+                double dblFuelConsumption; //Количество потребляемого топлива (л).
+
+                if (File.Exists(strPath) == true) //Если файл существует.
                 {
-                    Console.WriteLine("Воздушное судно:");
-                    Console.WriteLine($"{int1} - {DatabaseAirplane[int1].strName}");
-                    Console.WriteLine("     Параметры:");
-                    Console.WriteLine($"     Вместимость пассажиров (количество посадочных мест): {DatabaseAirplane[int1].intPassengerCapacity}");
-                    Console.WriteLine($"     Вместимость груза (кг): {DatabaseAirplane[int1].dblCargoCapacity}");
-                    Console.WriteLine($"     Дальность полета (км): {DatabaseAirplane[int1].dblFlightRange}");
-                    Console.WriteLine($"     Количество потребляемого топлива (л): {DatabaseAirplane[int1].dblFuelConsumption}");
-                }
-            }
+                    Array.Resize(ref arrstrDatabaseAirplane, 0);
+                    arrstrDatabaseAirplane = File.ReadAllLines(strPath);
 
+                    for (int int1 = 0; int1 <= arrstrDatabaseAirplane.Length - 1; int1++)
+                    {
+                        strName = arrstrDatabaseAirplane[int1]; //Название самолета (тип).
+                        int1++;
+                        intPassengerCapacity = Convert.ToInt32(arrstrDatabaseAirplane[int1]); //Вместимость пассажиров (количество посадочных мест).
+                        int1++;
+                        dblCargoCapacity = Convert.ToDouble(arrstrDatabaseAirplane[int1]); //Вместимость груза (кг).
+                        int1++;
+                        dblFlightRange = Convert.ToDouble(arrstrDatabaseAirplane[int1]); //Дальность полета (км).
+                        int1++;
+                        dblFuelConsumption = Convert.ToDouble(arrstrDatabaseAirplane[int1]); //Количество потребляемого топлива (л).
 
-
-
-            Console.WriteLine("Воздушное судно / Параметры:");
-
-            Console.WriteLine();
-
-
-
-            Console.WriteLine("Введите номер пункта меню:");
-            Console.WriteLine("1 - Добавление воздушного судна.");
-            Console.WriteLine("2 - Удаление воздушного судна.");
-            Console.WriteLine("3 - Переход в базу данных воздушных судов.");
-            Console.WriteLine("Exit - Выход в главное меню.");
-            Console.WriteLine();
-            Console.WriteLine("Авиакомпания:");
-            //Console.WriteLine(Airline[intNumberAirline].strName);
-            Console.WriteLine("     Воздушные суда:");
-            Console.WriteLine();
-
-
-
-
-
-            Console.Clear();
-            Console.WriteLine("Вы находитесь в разделе удаления авиакомпаний.");
-            Console.WriteLine();
-
-        }
-
-
-
-
-
-        public static void F_ReadDatabaseAirplane(ref List<claAirplane> DatabaseAirplane)
-        {
-            //Считывание базы данных самолетов из файла.
-            string strPath = @"d:\Registry\DatabaseAirplane.txt";
-            
-            string[] arrstrDatabaseAirplane = new string[0]; ;
-
-            string strName; //Название самолета (тип).
-            int intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
-            double dblCargoCapacity; //Вместимость груза (кг).
-            double dblFlightRange; //Дальность полета (км).
-            double dblFuelConsumption; //Количество потребляемого топлива (л).
-
-            if (File.Exists(strPath) == true) //Если файл существует.
-            {
-                Array.Resize(ref arrstrDatabaseAirplane, 0);
-                arrstrDatabaseAirplane = File.ReadAllLines(strPath);
-
-                for (int int1 = 0; int1 <= arrstrDatabaseAirplane.Length - 1; int1++)
-                {
-                    strName = arrstrDatabaseAirplane[int1]; //Название самолета (тип).
-                    intPassengerCapacity = Convert.ToInt32(arrstrDatabaseAirplane[int1++]); //Вместимость пассажиров (количество посадочных мест).
-                    dblCargoCapacity = Convert.ToDouble(arrstrDatabaseAirplane[int1++]); //Вместимость груза (кг).
-                    dblFlightRange = Convert.ToDouble(arrstrDatabaseAirplane[int1++]); //Дальность полета (км).
-                    dblFuelConsumption = Convert.ToDouble(arrstrDatabaseAirplane[int1++]); //Количество потребляемого топлива (л).
-
-                    DatabaseAirplane.Add(new claAirplane(strName, intPassengerCapacity, dblCargoCapacity, dblFlightRange, dblFuelConsumption));
+                        DatabaseAirplane.Add(new claAirplane(strName, intPassengerCapacity, dblCargoCapacity, dblFlightRange, dblFuelConsumption));
+                    }
                 }
             }
         }
 
 
-        //Считывание авиакомпаний из файла.
+        //Считываем авиакомпании из файла в коллекцию.
         public static void F_ReadAirline(ref List<claAirline> Airline)
         {
             string strPath;
@@ -375,6 +435,49 @@ namespace Airline
                 }
             }
         }
+
+
+        //Сохраняем базу данных воздушных судов из коллекции в файл.
+        public static void F_WriteDatabaseAirplane(List<claAirplane> DatabaseAirplane)
+        {
+            string strPath = @"d:\Registry\DatabaseAirplane";
+
+            if (Directory.Exists(strPath) == true) //Если директория существует.
+            {
+                Directory.Delete(strPath, true); //Удаляем.
+            }
+
+            Directory.CreateDirectory(strPath);
+
+            strPath = @"d:\Registry\DatabaseAirplane\DatabaseAirplane.txt";
+            
+            if (DatabaseAirplane.Count > 0)
+            {
+                for (int int1 = 0; int1 <= DatabaseAirplane.Count - 1; int1++)
+                {
+                    if(File.Exists(strPath) == true) //Если файл существует.
+                    {
+                        File.AppendAllText(strPath, "\n" + DatabaseAirplane[int1].strName); //Название самолета (тип).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].intPassengerCapacity)); //Вместимость пассажиров (количество посадочных мест).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblCargoCapacity)); //Вместимость груза (кг).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblFlightRange)); //Дальность полета (км).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblFuelConsumption)); //Количество потребляемого топлива (л).
+                    }
+                    else
+                    {
+                        File.AppendAllText(strPath, DatabaseAirplane[int1].strName); //Название самолета (тип).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].intPassengerCapacity)); //Вместимость пассажиров (количество посадочных мест).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblCargoCapacity)); //Вместимость груза (кг).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblFlightRange)); //Дальность полета (км).
+                        File.AppendAllText(strPath, "\n" + Convert.ToString(DatabaseAirplane[int1].dblFuelConsumption)); //Количество потребляемого топлива (л).
+                    }
+                }
+            }
+        }
+
+
+
+
 
 
 
