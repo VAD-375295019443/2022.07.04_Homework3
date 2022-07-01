9a43076ebc47c90d04a6bab6c6f459fb4366992a
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-07-01 12:54:02 +0300

    Changes

--- Temporary/Program.cs
+++ Temporary/Program.cs
@@ -6,27 +6,114 @@ namespace Temporary
     {
         public static void Main(string[] args)
         {
-            
+            List<claAirline> Airline = new List<claAirline>(); //Создаем коллекцию авиакомпаний.
 
-            int x = 2;
-            int y = 3;
+            Airline.Add(new claAirline("cccccccc"));
+            Airline.Add(new claAirline("aaaaaaaa"));
+            Airline.Add(new claAirline("bbbbbbbb"));
 
 
-            int z;
-            
-            z = summ(x, y);
-            Console.WriteLine(z);
-            
+            Airline[0].Airplane.Add(new claAirplane("yyyyyyyyyy", 99, 88, 22, 33));
+            Airline[0].Airplane.Add(new claAirplane("wwwww", 333, 111, 44, 11));
+            Airline[0].Airplane.Add(new claAirplane("qqqqqqqqqqqqqq", 55, 33, 22, 7));
+
+
+            Airline[1].Airplane.Add(new claAirplane("ssssssssssss", 45, 56, 78, 12));
+            Airline[1].Airplane.Add(new claAirplane("kkkkkkkkkkkkk", 13, 76, 8, 1));
+            Airline[1].Airplane.Add(new claAirplane("llllllllllll", 48, 8, 65, 3));
+
+
+            Airline[2].Airplane.Add(new claAirplane("yytytyt", 15, 6, 7, 2));
+            Airline[2].Airplane.Add(new claAirplane("sdds", 138, 776, 14, 1));
+            Airline[2].Airplane.Add(new claAirplane("yythkhkkytyt", 458, 478, 25, 13));
+
+            for (int int1=0; int1<=Airline.Count-1; int1++)
+            {
+                Console.WriteLine(Airline[int1].strName);
+
+                for (int int2=0; int2 <= Airline[int1].Airplane.Count-1; int2++)
+                {
+                    Console.WriteLine(Airline[int1].Airplane[int2].strName);
+                    Console.WriteLine(Airline[int1].Airplane[int2].intPassengerCapacity);
+                    Console.WriteLine(Airline[int1].Airplane[int2].dblCargoCapacity);
+                    Console.WriteLine(Airline[int1].Airplane[int2].dblFlightRange);
+                    Console.WriteLine(Airline[int1].Airplane[int2].dblFuelConsumption);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Console.WriteLine();
+
+
+
+
+            /*
+            var sortedPeople1 = Airline.OrderByDescending(x => x.strName)
+                .ThenBy(x => x.strName)
+                .ToList();
+            */
+
+            var sortedPeople1 = Airline.Where(x => x.strName == "cccccccc" || x.strName == "bbbbbbbb")
+                .ToList();
+
+
+
+
+
+
+            for (int int1 = 0; int1 <= sortedPeople1.Count-1; int1++)
+            {
+                Console.WriteLine(sortedPeople1[int1].strName);
+
+                for (int int2 = 0; int2 <= Airline[int1].Airplane.Count - 1; int2++)
+                {
+                    Console.WriteLine(sortedPeople1[int1].Airplane[int2].strName);
+                    Console.WriteLine(sortedPeople1[int1].Airplane[int2].intPassengerCapacity);
+                    Console.WriteLine(sortedPeople1[int1].Airplane[int2].dblCargoCapacity);
+                    Console.WriteLine(sortedPeople1[int1].Airplane[int2].dblFlightRange);
+                    Console.WriteLine(sortedPeople1[int1].Airplane[int2].dblFuelConsumption);
+                }
+            }
+
+
+
+
 
         }
 
 
 
-        public static int summ(int x, int y)
+        //Класс воздушных судов.
+        public class claAirplane
         {
-            int a = 0;
-            a = x + y;
-            return a;
+            public string strName; //Название самолета (тип).
+            public int intPassengerCapacity; //Вместимость пассажиров (количество посадочных мест).
+            public double dblCargoCapacity; //Вместимость груза (кг).
+            public double dblFlightRange; //Дальность полета (км).
+            public double dblFuelConsumption; //Количество потребляемого топлива (л).
+
+            public claAirplane(string strName, int intPassengerCapacity, double dblCargoCapacity, double dblFlightRange, double dblFuelConsumption)
+            {
+                this.strName = strName;
+                this.intPassengerCapacity = intPassengerCapacity;
+                this.dblCargoCapacity = dblCargoCapacity;
+                this.dblFlightRange = dblFlightRange;
+                this.dblFuelConsumption = dblFuelConsumption;
+            }
+        }
+
+
+        //Класс авиакомпаний.
+        public class claAirline
+        {
+            public string strName; //Название авиакомпаний.
+            public List<claAirplane> Airplane = new List<claAirplane>(); //Создаем коллекцию самолетов.
+
+            public claAirline(string strName)
+            {
+                this.strName = strName;
+            }
         }
 
 
